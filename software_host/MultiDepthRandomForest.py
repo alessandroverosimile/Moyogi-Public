eed50745a2f30af0c8d7475fa233673da59d48bf
alessandroverosimile <alessandro.verosimile@polimi.it>
2025-09-01 10:19:05 +0000

    update to MultiDepthRandomForest to speedup NN input preparations during training

--- software_host/MultiDepthRandomForest.py
+++ software_host/MultiDepthRandomForest.py
@@ -8,6 +8,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.get_logger().setLevel('ERROR')
 tf.keras.config.disable_interactive_logging()
 import tensorflow as tf
+from tqdm.notebook import tqdm
+from sklearn.metrics import confusion_matrix
 np.random.seed(42)
 
 class MultiDepthRandomForestClassifier:
@@ -65,10 +67,10 @@ class MultiDepthNeuralRandomForestClassifier:
         seed = 1234
         tf.random.set_seed(seed)
         input = tfkl.Input(shape=input_shape)
-        output = tfkl.Conv1D(1,len(self.classifiers.keys()),padding='valid',kernel_initializer = tfk.initializers.Ones(),kernel_regularizer=tfk.regularizers.L1L2(lambda_reg,lambda_reg),activation='relu',strides=len(self.classifiers.keys()))(input)
+        output = tfkl.Conv1D(1,len(self.classifiers.keys()),padding='valid',kernel_initializer = tfk.initializers.Ones(),kernel_regularizer=tfk.regularizers.L1L2(lambda_reg,lambda_reg),activation='relu',strides=len(self.classifiers.keys()), kernel_constraint = tfk.constraints.NonNeg(), name='conv1D')(input)
         output = output[:,:,0]
         model = tfk.Model(inputs=input, outputs=output, name='model')
-        model.compile(loss=tfk.losses.CategoricalCrossentropy(from_logits=True), optimizer=tfk.optimizers.Adam(1e-3), metrics='accuracy')
+        model.compile(loss=tfk.losses.CategoricalCrossentropy(from_logits=True), optimizer=tfk.optimizers.Adam(1e-3), metrics=['accuracy'])
         return model
 
     def to_ohe(self,y,n_classes):
@@ -77,56 +79,34 @@ class MultiDepthNeuralRandomForestClassifier:
             y_ohe[i,int(y[i])] = 1
         return y_ohe
 
-    def fit(self, X_train, y_train, X_val, y_val):
+    def fit(self, X_train, y_train, X_val, y_val, patience):
         
         self.n_classes = int(np.max(y_train)+1)
         assert (self.max_depth+1-self.min_depth)%2 == 1, 'The difference between max and min depth must be even'
+        
         for i,depth in enumerate(range(self.min_depth,self.max_depth+1)):
+            #print(f"Training Random Forest {i+1}")
             classifier = RandomForestClassifier(n_estimators=self.estimator_per_depth[i],max_depth=depth,random_state=self.random_state)
             classifier.fit(X_train,y_train)
             self.classifiers[depth] = classifier
         
-        samples = []
-        for i in range(len(X_train)):
-            sample = []
-            predictions = []
-            for classifier in self.classifiers.values():
-                prediction = classifier.predict_proba(X_train[i:i+1])[0]
-                predictions.append(prediction)
+        samples = np.zeros((X_train.shape[0],self.n_classes*(self.max_depth-self.min_depth+1)))
+        for i,classifier in enumerate(self.classifiers.values()):
+            prediction = classifier.predict_proba(X_train)
+            for j in range(prediction.shape[1]):
+                samples[:,j*len(self.classifiers.values())+i] = prediction[:,j]
 
-            predictions = np.array(predictions)
-            
-            for i in range(len(predictions[0])):
-                for j in range(len(predictions)):
-                    sample.append(predictions[j,i])
+        samples_val = np.zeros((X_val.shape[0],self.n_classes*(self.max_depth-self.min_depth+1)))
+        for i,classifier in enumerate(self.classifiers.values()):
+            prediction = classifier.predict_proba(X_val)
+            for j in range(prediction.shape[1]):
+                samples_val[:,j*len(self.classifiers.values())+i] = prediction[:,j]
 
-            samples.append(sample)
-        samples = np.array(samples)
-
-        samples_val = []
-        for i in range(len(X_val)):
-            sample = []
-            predictions = []
-            for classifier in self.classifiers.values():
-                prediction = classifier.predict_proba(X_val[i:i+1])[0]
-                predictions.append(prediction)
-
-            predictions = np.array(predictions)
-            
-            for i in range(len(predictions[0])):
-                for j in range(len(predictions)):
-                    sample.append(predictions[j,i])
-
-            samples_val.append(sample)
-        samples_val = np.array(samples_val)
-
-        n_classes = int(np.max(y_train)+1)
-
-        y_train_ohe = self.to_ohe(y_train,n_classes)
-        y_val_ohe = self.to_ohe(y_val,n_classes)
+        y_train_ohe = self.to_ohe(y_train,self.n_classes)
+        y_val_ohe = self.to_ohe(y_val,self.n_classes)
         
         model = self.build_model((self.n_classes*len(self.classifiers.keys()),1),0)
-
+        #print("training NN")
         history = model.fit(
             x = samples,
             y = y_train_ohe,
@@ -134,29 +114,21 @@ class MultiDepthNeuralRandomForestClassifier:
             epochs = 5000,
             validation_data = (samples_val, y_val_ohe),
             callbacks = [
-                tfk.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=25, restore_best_weights=True),
+                tfk.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=patience, restore_best_weights=True),
             ]
         ).history
 
-        predictions = model.predict(samples_val)
+        #xpredictions = model.predict(samples_val)
 
         self.model = model
     
     def predict_proba(self,X_test):
-        samples = []
-        for i in range(len(X_test)):
-            sample = []
-            predictions = []
-            for classifier in self.classifiers.values():
-                prediction = classifier.predict_proba(X_test[i:i+1])[0]
-                predictions.append(prediction)
-            predictions = np.array(predictions)
-
-            for i in range(len(predictions[0])):
-                for j in range(len(predictions)):
-                    sample.append(predictions[j,i])
-            
-            samples.append(sample)
+   
+        samples = np.zeros((X_test.shape[0],self.n_classes*(self.max_depth-self.min_depth+1)))
+        for i,classifier in enumerate(self.classifiers.values()):
+            prediction = classifier.predict_proba(X_test)
+            for j in range(prediction.shape[1]):
+                samples[:,j*len(self.classifiers.values())+i] = prediction[:,j]     
         
         samples = np.array(samples)
         predictions_nn = self.model.predict(samples)
@@ -169,3 +141,10 @@ class MultiDepthNeuralRandomForestClassifier:
     def score(self,X_test,y_test):
         predictions = self.predict(X_test)
         return np.sum(predictions==y_test)/len(predictions)
+    
+    def all_metrics(self,X_test,y_test):
+        predictions = self.predict(X_test)
+        probs = self.predict_proba(X_test)
+        accuracy = np.sum(predictions==y_test)/len(predictions)
+        cm = confusion_matrix(y_test, predictions)
+        return accuracy, cm, probs
